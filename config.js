83572f019661baa1e81ba6c54c7eb8d1ce970413
mkeedlinger <mkeedlinger@gmail.com>
2014-08-27 14:08:35 -0700

    oops, i committed a password?? good luck using it now.. :)

--- config.js
+++ config.js
@@ -1,32 +1,9 @@
 // depends
 var colors = require('colors');
 
-module.exports = {
-    port: process.env.PORT || 3000,
-    root: __dirname,
-    prod: !!process.env.PROD || false,
-    sendgrid: {
-        user: 'christiansenfasteners',
-        pass: 'eakentuituel'
-    }
-}
-def = module.exports;
-
 console.l = function () {
-    if (!def.prod) {
-        return function () {
-            var logs = Array.prototype.slice.call(arguments),
-                logTag = '[debug] ',
-                logMessage = [logTag['yellow'] + logs[0]].concat(logs.slice(1));
-            console.log.apply(console, logMessage);
-        }
-    } else {
-        return function () {}
-    }
-}();
-console.ll = function () {
     var logs = Array.prototype.slice.call(arguments),
-        logTag = '[lock-up] ',
-        logMessage = [logTag['green'] + logs[0]].concat(logs.slice(1));
+        logTag = '[debug] ',
+        logMessage = [logTag['yellow'] + logs[0]].concat(logs.slice(1));
     console.log.apply(console, logMessage);
 }
